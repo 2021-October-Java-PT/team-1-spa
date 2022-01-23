deeb233026f4fb420f94a550df497108522a7d06
LyzzHans <lyzz.hans@gmail.com>
2022-01-23 15:08:30 -0500

    start to populator

---
+++ server-side/src/main/java/org/wecancodeit/serverside/Populator.java
@@ -0,0 +1,11 @@
+package org.wecancodeit.serverside;
+
+public class Populator {
+
+    Website scratch = new website("Scratch", "Scratch is the world's largest free coding community for kids.", "https://scratch.mit.edu/");
+    Website codeOrg = new website("code.org", "Code.org® is a nonprofit dedicated to expanding access to computer science in schools and increasing participation by young women and students from other underrepresented groups.", "https://code.org/");
+    Website codeCombat = new website("Code Combat", "CodeCombat is acommunity project,with hundreds of players volunteering to create levels, contribute to our code to add features, fix bugs, playtest, and even translate the game into 50 languages so far.", "https://codecombat.com/home");
+    Website blockyGames = new website("BlockyGames", "Blocky offers a variety of games adopting the approach of teaching kids how to code using interactive challenges", "https://blockly.games");
+    Website crunchzilla = new website("crunchzilla", "Crunchzilla is one of the simplest teaching platforms when it comes to boosting your kids' knowledge of coding.", "http://www.crunchzilla.com/code-monster");
+
+
