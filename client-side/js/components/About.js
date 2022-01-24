b918f55c35c40941d8a46a807b8e08429227c5cd
nichofer <nvasaris@kent.edu>
2022-01-24 18:49:12 -0500

    about-us

---
+++ client-side/js/components/About.js
@@ -0,0 +1,78 @@
+import Nicole from "../../images/Nicole.jpg";
+import gitHubImg from "../../images/GitHub.png";
+import linkedInImg from "../../images/LinkedIn.png";
+
+export default function About() {
+  return `
+    <div class="container-fluid api-button text-center">    
+    <h3 class="margin" id="">ABOUT US</h3><br>
+    <main class="bios">
+        <section id="lyzzhans">
+            <h2>Lyzz Hans</h2>
+            <img class="headshot" src="" alt="Headshot" />
+            <p class>
+               
+            </p>
+            <a id="github" href="">
+                <img  id="github" src="${gitHubImg}" alt="github" />
+            </a>
+            <a id="linkedin" href="">
+                <img id="linkedin" src="${linkedInImg}" alt="linkedin" />
+            </a>
+            </section>
+        <section id="nicolehofer">
+          <h2>Nicole Hofer</h2>
+          <img class="headshot" src="${Nicole}" alt="Headshot" />
+          <p class>
+            A rising software developer permanently curious about technology.
+            Currently specializes in front-end applications and robotics. During
+            free time, enjoys working on and building a homestead.
+          </p>
+          <a id="github" href="https://github.com/nichofer">
+            <img  id="github" src="${gitHubImg}" alt="github" />
+          </a>
+          <a id="linkedin" href="http://linkedin.com/in/nicole-hofer">
+            <img id="linkedin" src="${linkedInImg}" alt="linkedin" />
+          </a>
+        </section>
+        <section id="madisonmitchell">
+          <h2>Madison Mitchell</h2>
+          <img class="headshot" src="" alt="Headshot" />
+          <p class>
+            
+          </p>
+          <a id="github" href="">
+            <img  id="github" src="${gitHubImg}" alt="github" />
+          </a>
+          <a id="linkedin" href="">
+            <img id="linkedin" src="${linkedInImg}" alt="linkedin" />
+          </a>
+        </section>
+        <section id="diascruggs">
+          <h2>Dia Scruggs</h2>
+          <img class="headshot" src="" alt="Headshot" />
+          <p class>
+            
+          </p>
+          <a id="github" href="">
+            <img  id="github" src="${gitHubImg}" alt="github" />
+          </a>
+          <a id="linkedin" href="">
+            <img id="linkedin" src="${linkedInImg}" alt="linkedin" />
+          </a>
+        </section>
+        <section id="josephinewilson">
+          <h2>Josephine Wilson</h2>
+          <img class="headshot" src="" alt="Headshot" />
+          <p class>
+            
+          </p>
+          <a id="github" href="">
+            <img  id="github" src="${gitHubImg}" alt="github" />
+          </a>
+          <a id="linkedin" href="">
+            <img id="linkedin" src="${linkedInImg}" alt="linkedin" />
+          </a>
+        </section>        
+    </div>`;
+}
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,19 @@
-console.log('Client Side is wired up!');
+// console.log('Client Side is wired up!');
+
+import About from "./components/About";
+
+const app = document.querySelector("#app");
+
+buildPage();
+
+function buildPage() {
+  about();
+}
+
+function about() {
+  const aboutElem = document.querySelector(".navbar-about");
+  aboutElem.addEventListener("click", () => {
+    // console.log("button clicked");
+    app.innerHTML = About();
+  });
+}
