b6295dc68eeacb34db18d0a379cd216de0631e03
DiaS793 <DiaScruggs793@outlook.com>
2022-01-25 18:04:21 -0500

    added contact page to index

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,6 +1,7 @@
 // console.log('Client Side is wired up!');
 
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 const app = document.querySelector("#app");
 
@@ -8,6 +9,7 @@ buildPage();
 
 function buildPage() {
   about();
+  navContact();
 }
 
 function about() {
@@ -15,5 +17,12 @@ function about() {
   aboutElem.addEventListener("click", () => {
     // console.log("button clicked");
     app.innerHTML = About();
+  });
+}
+
+function navContact() {
+  const contactElem = document.querySelector(".navbar-contact");
+  contactElem.addEventListener("click", () => {
+    app.innerHTML = Contact();
   });
 }
