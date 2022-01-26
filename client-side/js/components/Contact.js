a848b90cf6cddf7456c14909582d6ae53384bab2
DiaS793 <DiaScruggs793@outlook.com>
2022-01-25 23:56:13 -0500

    updated contact page with form

--- client-side/js/components/Contact.js
+++ client-side/js/components/Contact.js
@@ -1,11 +1,16 @@
 export default function Contact() {
   return `
+  
   <div class="container-fluid api-button text-center"> 
-    <h1>Contact us </h1>
-    <article>
-        <h4>Contact Us Today!</h4>
-        <p> (555) 555-5555</p>
-    </article>
-    </div>
+  <h1> Give us your feed back, please! </h1>
+  <div class=box>
+  <div class="wrapperContact">
+    <input type="text" class="boxContact box1Contact" name="firstname" placeholder="Your first name here">
+    <input type="text" class="boxContact box2Contact" name="lastname" placeholder="Your last name here">
+    <input type="text" class="boxContact box3Contact" name="yourEmail" placeholder="Your email, please.."/>
+    <textarea name="subject" class="boxContact box4Contact" placeholder="Write your thoughts here.." cols="30" rows="10"></textarea>
+    <input class="boxContact box5Contact" type="submit" value="Submit"/>
+  </div>
+  </div>
     `;
 }
