24949109ddae29773f32b3f39e5bc882aea99f53
nichofer <nvasaris@kent.edu>
2022-01-26 18:01:00 -0500

    About updates

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -1,5 +1,7 @@
+import Dia from "../../images/Dia.jpg";
 import Lyzz from "../../images/Lyzz.png";
-import Nicole from "../../images/Nicole.jpg";
+import Madison from "../../images/Madison1.jpg";
+import Nicole from "../../images/Nicole1.png";
 import gitHubImg from "../../images/GitHub.png";
 import linkedInImg from "../../images/LinkedIn.png";
 
@@ -11,13 +13,15 @@ export default function About() {
         <section id="lyzzhans">
             <h2>Lyzz Hans</h2>
             <img class="headshot" src="${Lyzz}" alt="Headshot" />
-            <p class>
-               
+            <p class>I'm a coding hippie. Two things fascinate me: people and coding. I am constantly curious about our interconnectivity and a tireless advocate for inclusiveness and diversity. 
+            During the pandemic, I enrolled in a Software Development Bootcamp and learned that I am obsessed with learning all things code and that test driven development is my jam.
+            My enthusiasm for people and coding transpires well in Agile environments. I am always willing to help a fellow developer with questions and advice and I love sparking joy in teams.
+            As a previous educator for adult learners, I am an avid believer that we are all life long learners. I transition quickly into any team because I am ready to work, but also ready to learn from my team and mentors.
             </p>
-            <a id="github" href="">
+            <a id="github" href="https://github.com/LyzzHans">
                 <img  id="github" src="${gitHubImg}" alt="github" />
             </a>
-            <a id="linkedin" href="">
+            <a id="linkedin" href="https://linkedin.com/in/lyzzhans/">
                 <img id="linkedin" src="${linkedInImg}" alt="linkedin" />
             </a>
             </section>
@@ -38,9 +42,13 @@ export default function About() {
         </section>
         <section id="madisonmitchell">
           <h2>Madison Mitchell</h2>
-          <img class="headshot" src="" alt="Headshot" />
-          <p class>
-            
+          <img class="headshot" src="${Madison}" alt="Headshot" />
+          <p class>A highly motivated Software Developer with 9 years experience working in the Technology Industry. 
+          Currently working for a Fortune 500 Tech Company as a four-consecutive-year Triple Exceeding Expectations Tech Advisor 
+          that is dedicated to producing high quality products and results. Naturally curious with a strong desire to grow skill set. 
+          Excels in resolving challenges encountered in the workplace with innovative solutions and passionate about creating high quality, creative products.
+          An avid reader, particuarly fantasy novels, and enjoys working on side projects to continue to challenge oneself as well as create handy products around the house, 
+          like a smart mirror that tells the time, current events and weather updates.
           </p>
           <a id="github" href="https://github.com/madison-mitchell">
             <img  id="github" src="${gitHubImg}" alt="github" />
@@ -51,7 +59,7 @@ export default function About() {
         </section>
         <section id="diascruggs">
           <h2>Dia Scruggs</h2>
-          <img class="headshot" src="" alt="Headshot" />
+          <img class="headshot" src="${Dia}" alt="Headshot" />
           <p class>
             
           </p>
