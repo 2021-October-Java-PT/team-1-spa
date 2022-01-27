3a137cdb6505b06ab1e2e2e66c059dac58408bd1
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-01-27 10:55:51 -0500

    Wired up server-side; Created skeleton for Technology.js file; Added in function to render Programing Resources in app div when clicking on Technology tile

--- server-side/src/main/java/org/wecancodeit/serverside/rest/controllers/ProgrammingRestController.java
+++ server-side/src/main/java/org/wecancodeit/serverside/rest/controllers/ProgrammingRestController.java
@@ -3,8 +3,8 @@ package org.wecancodeit.serverside.rest.controllers;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
-import org.wecancodeit.serverside.repositories.ProgrammingRepository;
 import org.wecancodeit.serverside.models.ProgrammingResource;
+import org.wecancodeit.serverside.repositories.ProgrammingResourceRepository;
 
 import javax.annotation.Resource;
 import java.util.Collection;
@@ -15,30 +15,30 @@ import java.util.Optional;
 public class ProgrammingRestController {
 
     @Resource
-    private ProgrammingRepository programmingRepo;
+    private ProgrammingResourceRepository programmingRepo;
 
-    @GetMapping("/api/programming-languages")
+    @GetMapping("/api/programming-resources")
     public Collection<ProgrammingResource> getProgrammingLanguages() {
         return (Collection<ProgrammingResource>) programmingRepo.findAll();
     }
 
-    @GetMapping("/api/programming-languages/{id}")
+    @GetMapping("/api/programming-resources/{id}")
     public Optional<ProgrammingResource> getProgrammingLanguage(@PathVariable Long id) {
         return programmingRepo.findById(id);
     }
 
-    @PostMapping("/api/programming-languages/add-language")
+    @PostMapping("/api/programming-resources/add-resource")
     public Collection<ProgrammingResource> addItem(@RequestBody String body) throws JSONException {
         JSONObject newLanguage = new JSONObject(body);
-        String name = newLanguage.getString("language name");
-        String description = newLanguage.getString("language description");
+        String name = newLanguage.getString("resource name");
+        String description = newLanguage.getString("resource description");
         String educationWebsiteUrl = newLanguage.getString("website URL");
 
-        Optional<ProgrammingResource> languageToAddOpt = programmingRepo.findByName(name);
+        Optional<ProgrammingResource> resourceToAddOpt = programmingRepo.findByName(name);
 
-        if (languageToAddOpt.isEmpty()) {
-            ProgrammingResource languageToAdd = new ProgrammingResource(name, description, educationWebsiteUrl);
-            programmingRepo.save(languageToAdd);
+        if (resourceToAddOpt.isEmpty()) {
+            ProgrammingResource resourceToAdd = new ProgrammingResource(name, description, educationWebsiteUrl);
+            programmingRepo.save(resourceToAdd);
         }
 
         return (Collection<ProgrammingResource>) programmingRepo.findAll();
