d7a3332d183114aee07e35354697d14599dfc980
DiaS793 <DiaScruggs793@outlook.com>
2022-01-30 10:43:18 -0500

    updated art

--- client-side/js/components/Art.js
+++ client-side/js/components/Art.js
@@ -1,5 +1,41 @@
-export default function Art(art){
+export default function Art1(metObject) {
     return `
-    
-    `;
-}
+          
+          <div class="background">
+          <h2 id="lingaTitle">${metObject.department}<h2>
+          <section class="linga-flex">
+          <img src="${metObject.primaryImageSmall}" id="lingaImg" >
+          <section>
+          <h2>${metObject.objectDate} Hindu Art Piece</h2>
+          <h2>${metObject.repository}</h2>
+          </section>
+          <img src="${metObject.additionalImages}" id="lingaImg2" >
+          <section>
+          </section>
+          
+          </section>
+  
+          <h2 id="lingaPiece">${metObject.title}</h2>
+          <p id="linga" > 
+          <br> This is a piece held in the New York Metropolitan Museum called a ${metObject.title}. It was founded in the 15th century which was a 
+          really long time ago in Nepal, a country in South Asia. This is representative of the pillar of fire that the most powerful god in the
+          Hindu religion, Shiva, came from. According to the <a href="https://www.britannica.com/topic/lingam"> Britannica</a> the Linga is votary 
+          or dedication to Shiva.This Linga covering has the four faces of Shiva carved onto it.  <br>
+          Now you may be wondering, who is Shiva? Well according to the Hindu faith, Shiva is the most powerful god among the gods in this religion. His origin 
+          story beings long ago when two gods of the Hindu faith, the creator of the universe, Brahma, and the protector of the universe, Vishnu, got 
+          into a fight over who was the most powerful god among them. During that fight, a lingam or pillar of fire appeared between them and it was said to
+          straight through the earth and into the universe itself. <br>
+          When this happened, the gods decided to race to see who could get to the end of the pillar first and that would decide who was the most powerful.
+          Vishnu decided to go through the earth while Brahma went through the universe. However, either gods got to the end of the lingam. <br>
+          Brahma, however, did not want to lose and so when he saw a flower during his journey up the lingam, he picked it up and went back down the pillar to
+          tell a lie to Vishnu that he had made it to the top of the lingam. This of course upset Vishnu and they were about to fight again when suddenly,
+          the Ligma opened up! <br>
+          Inside of it was Shiva. He revealed himself to be the most powerful god and punished Brahma for lying by cursing him to never have anyone
+          pray to him again. This tail also marks a special holiday for this religion called Shivratri which celebrates Shiva.
+          
+          </p>
+          </div>
+          
+        `;
+  }
+  
