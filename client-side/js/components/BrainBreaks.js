e741521f63a0a768aa9c09e20c9e4233675807dc
nichofer <nvasaris@kent.edu>
2022-01-30 10:57:12 -0500

    Brain break updates

--- client-side/js/components/BrainBreaks.js
+++ client-side/js/components/BrainBreaks.js
@@ -36,15 +36,6 @@ export default function BrainBreaks() {
     </div>`;
   }
 
-// accessibility -A factor describing how possible an event is to do with zero being the most accessible
-// [0.0, 1.0]
+//Boredom API https://www.boredapi.com/documentation
 
-// type - Type of the activity
-// ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
-
-// participants - The number of people that this activity could involve
-// [0, n]
-
-// price - A factor describing the cost of the event with zero being free
-// [0, 1]
 
