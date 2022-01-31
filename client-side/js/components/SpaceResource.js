0d5e788dc3836aa0decdb09434d62b7cc26d9e62
LyzzHans <lyzz.hans@gmail.com>
2022-01-31 18:55:45 -0500

    updated picture

--- client-side/js/components/SpaceResource.js
+++ client-side/js/components/SpaceResource.js
@@ -2,9 +2,9 @@ export default function SpaceResource(spaceResource) {
     return `
   
     <h1>Mary Jackson:</h1>
-    <img src= ${spaceResource.collection.items[58].href}></img>
+    <img src= ${spaceResource.collection.items[40].links[0].href}></img>
     <h2>${spaceResource.collection.items[58].data[0].description}</h2> 
-    <img src =${spaceResource.collection.items[23].href}</img>
+    
     <button class="returnScience">Return to Science</button>
    
     </div>
