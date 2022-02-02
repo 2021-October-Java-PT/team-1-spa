92fc7e40897ed2503b74876835afa43dcf155185
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-02-02 09:47:35 -0500

    WIP: Attempting to resolve error when grabbing MediaSection DIV in xtraResource.js file

--- client-side/js/components/XtraResource.js
+++ client-side/js/components/XtraResource.js
@@ -1,11 +1,44 @@
-export default function XtraResource () {
+import apiHelpers from "../api-helpers/apiHelpers.js";
+
+export default function XtraResource(xtraResource) {
+
+    const mediaSection = document.getElementById("media-section");
+
+    if (xtraResource.media_type == "video") {
+        const videoSection = `
+        <div class="video-div">
+        <iframe id="videoLink" width="500" src=""></iframe>
+        </div>`;
+        console.log(mediaSection);
+        mediaSection.innerHTML = videoSection;
+        document.getElementById("videoLink").src = xtraResource.url;
+        console.log(mediaSection);
+        
+    } else {
+        const imageSection = `
+        <a id="NASAImg" href="" target="-blank">
+        <div class="image-div">
+        <img id="image_of_the_day" src="${xtraResource.url}" alt="image-by-nasa">
+        </div>
+        </a></div>`;
+        mediaSection.innerHTML = imageSection;
+        console.log(mediaSection);
+        document.getElementById("NASAImg").href = xtraResource.hdurl;
+        document.getElementById("image_of_the_day").src = xtraResource.url;
+    }
+
+    apiHelpers.getRequest("https://api.nasa.gov/planetary/apod?api_key=eWhcVkX9a7jqZ58hERTeYYEoHEdjjXN5gea5XwRC", (xtraResource) => {
+        displayVideo(xtraResource);
+    })
+
     return `
     <h1>NASA Picture of the Day:</h1>
     <h2>Date: ${xtraResource.date}</h2>
     <h3>Title: ${xtraResource.title}</h3>
     <p>${xtraResource.explanation}</p>
     <a href="${xtraResource.url}</a>
-    
-    
+    <div id="media-section">
+    </div>
+       
     `;
 }
