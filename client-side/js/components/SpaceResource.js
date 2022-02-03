22c962dc03398f45ad4a6cc432347f11d327ec06
nichofer <nvasaris@kent.edu>
2022-02-03 12:33:16 -0500

    Science updates

--- client-side/js/components/SpaceResource.js
+++ client-side/js/components/SpaceResource.js
@@ -1,7 +1,11 @@
-//import apiHelpers from "../api-helpers/apiHelpers.js";
-
-export default function SpaceResource(spaceResource){
-    return`
-    <h1>Test</h1>
-   `;  
+export default function SpaceResources(spaceResource) {
+    console.log('SPACE RESOURCE', spaceResource);
+    let i = Math.floor(Math.random() * 10);
+    console.log(i);
+    return `
+    <h1>1, 2, 3 Blast Off!</h1>
+    <img src="${spaceResource.collection.items[i].links[0].href}"></img>
+    <h2>${spaceResource.collection.items[i].data[0].description}</h2>
+    <button class="returnScience btn btn-info">Return</button>
+    `;
 }
