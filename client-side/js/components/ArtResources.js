892e0376a5e95cfba72c662683abd3814dd407d6
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-02-03 22:16:06 -0500

    Added styling to art tiles

--- client-side/js/components/ArtResources.js
+++ client-side/js/components/ArtResources.js
@@ -4,15 +4,14 @@ export default function ArtResources() {
   return `
     <div class="api-topics">
         <ul>     
-            <li class="btn btn-info"><img src="${artBox}" class="artMask">
+            <li class="btn btn-info topic-list"><img src="${artBox}" class="artMask topic-tile">
                 <input type="hidden" id="artId" value=""></li>
-            <li class="btn btn-info"><img src="${artBox}" class="artMusic">
+            <li class="btn btn-info topic-list"><img src="${artBox}" class="artMusic topic-tile">
                 <input type="hidden" id="artId" value=""></li>
-            <li class="btn btn-info"><img src="${artBox}" class="artArch">
+            <li class="btn btn-info topic-list"><img src="${artBox}" class="artArch topic-tile">
                 <input type="hidden" id="artId" value=""></li>
-            <li class="btn btn-info"><img src="${artBox}" class="artStat">
+            <li class="btn btn-info topic-list"><img src="${artBox}" class="artStat topic-tile">
                 <input type="hidden" id="artId" value=""></li>
-
         </ul>
            
     </div>    
