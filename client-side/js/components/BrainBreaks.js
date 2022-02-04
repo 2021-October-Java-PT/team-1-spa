f6cb91e31502a18ea799ea17018c934a9ee0c930
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-02-03 21:49:59 -0500

    Push to resolve weird conflicts

--- client-side/js/components/BrainBreaks.js
+++ client-side/js/components/BrainBreaks.js
@@ -1,13 +1,3 @@
-import busywork from "../../images/busywork-box.png";
-import charity from "../../images/charity-box.png";
-import cooking from "../../images/cooking-box.png";
-import diy from "../../images/diy-box.png";
-import education from "../../images/education-box.png";
-import music from "../../images/music-box.png";
-import recreation from "../../images/recreation-box.png";
-import relax from "../../images/relax-box.png";
-import social from "../../images/social-box.png";
-
 export default function BrainBreaks() {
     return `
     <div class="api-page-container">
